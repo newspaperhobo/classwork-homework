ffe52f05f291c975948a270cdd9172f1ff32ca9e
newspaperhobo <hoboinabox@gmail.com>
2021-03-13 14:09:19 -0500

    initial commit

---
+++ 4.1-p3-cw-math-object-methods/scripts.js
@@ -0,0 +1,27 @@
+console.log("Testing... Hey, this works!")
+
+// 1
+// Print to the console the ceil value of 41.2
+
+console.log(Math.ceil(41.2));
+
+// 2
+// Print to the console the floored value of 29.6
+console.log(Math.floor(29.6));
+
+
+// 3
+// Print to the console the rounded value of -93.5. What Math object method can we use to do this?
+console.log(Math.floor(-93.5))
+
+
+// 4
+// Print to the console the largest value in this list of numbers: 90.5, 23, -12, 78, 7
+// Note: Numbers need to passed in this order
+console.log(Math.max(90.5, 23, -12, 78, 7));
+
+
+// 5
+// Print to the console a random number between 6 and 11
+
+console.log(Math.floor(Math.random() * (11 - 6 + 1)) + 6);
