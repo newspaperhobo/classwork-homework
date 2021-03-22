2c78c86154a3b812417e0690bb0fbf8b9352550f
newspaperhobo <hoboinabox@gmail.com>
2021-03-22 17:35:45 -0400

    initial commit

---
+++ 4.3-temp-convert-hw/script/script.js
@@ -0,0 +1,60 @@
+/*  
+Temperature conversion app.
+
+Part 1
+Link this script file to the HTML file. Confirm it is linked by
+console logging something.
+
+
+
+Part 2
+Create 2 HTML text elements in the HTML file so that we can display
+our outputs to the user.
+
+Part 3
+Ask the user for the temperature in Fahrenheit.
+Convert the number to Celcius and display it back to the user.
+
+Part 4
+If the degrees in Fahrenheit is less than 69, display a message
+that says "Oh that's cold!".
+If the degrees in Fahrenheit is greater than or equal to 69, the message
+should say "Temperature is just right!".
+Otherwise the message should say "That's not a temperature".
+
+---
+Equation is: °C = 5 / 9(°F - 32).
+
+First think about each step, write your own notes about what to do, 
+and then start coding. Remember to test your code throughout the 
+whole process by checking the browser and the console.
+
+For hints, check out the hints.txt file.
+*/
+
+console.log("sup world");
+let tempF = Number(prompt("What's the temp in the USA, ranger?"));
+function convertFarenheitToCelcius(tempF) {
+    let tempC = Math.round((tempF - 32) * (5 / 9))
+    return tempC;
+}
+
+if (isNaN(tempF)) {
+    document.getElementById("tempC").innerHTML = `That's not a temperature! You didn't enter a number, please refresh the page!`
+} else {
+    let tempC = convertFarenheitToCelcius(tempF);
+    console.log(tempC);
+    document.getElementById("tempC").innerHTML = `The temperature is ${tempC} degrees Celcius, chap!`
+}
+
+function getTemp(tempF) {
+    if (tempF < 69) {
+        document.getElementById("tempReact").innerHTML = "Ooo, that's chilly, dawg!"
+    } else if (tempF >= 69) {
+        document.getElementById("tempReact").innerHTML = "Wowie, it's just the right temp, buddy!"
+    }
+    return tempF
+}
+
+getTemp(tempF);
+
