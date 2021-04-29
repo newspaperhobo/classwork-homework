6d79c1ca8e1324a1b6041dcd69545ca0465f8a43
newspaperhobo <hoboinabox@gmail.com>
2021-04-28 21:47:16 -0400

    initial commit

--- 5.0-p2-cw-node-project/app.js
+++ 5.0-p2-cw-node-project/app.js
@@ -6,7 +6,7 @@ const PORT = 3000;
 
 app.get("/", (request, response) => {
     response.send("What's up!");
-})
+}) 
 
 app.listen(PORT, () => {
     console.log(`The server is listening on port ${PORT}`)
